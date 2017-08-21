512175944685207ce49861ba0a2ad0cd0ebd2d16
waliag87 <gwalia@Gurinders-MacBook-Pro.local>
2017-08-21 13:10:03 -0700

    article parser class complete

--- src/ArticleParser.java
+++ src/ArticleParser.java
@@ -1,26 +1,80 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.URL;
 
-public class ArticleParser{
+public class ArticleParser {
     /**
-     *  Attempting to parse a single article from site
-     *
+     * Attempting to parse a single article from site
      */
-    public static void main(String [] args){
-
-        // Get's html from site and makes a "Document"
-       try{
-           Document doc =  Jsoup.connect("https://jsoup.org/cookbook/input/load-document-from-url").get();
-           String title = doc.title();
-       }catch(IOException e){
-           System.out.println("IO Exception Thrown from creating Document");
-       }
+    private String articleURL;
+    private Document doc;
 
 
+    public ArticleParser(String articleURL) {
+        this.articleURL = articleURL;
+    }
 
+    /**
+     * create a jsoup Document
+     *
+     * @return jsoup Document created from article's URL
+     */
+    public void getArticleText() throws java.io.IOException {
+        this.doc = Jsoup.connect(this.articleURL).get();
+    }
 
+    /**
+     * Method to get title of the article
+     *
+     * @return title of the article
+     */
+    public String getArticleTitle() {
+        return doc.title();
+
+    }
+
+    /**
+     * Get the titles of the events from the article
+     *
+     * @return the titles of all the events as an Elements type
+     */
+    public Elements getEventNames() {
+        Elements content = doc.getElementsByClass("post content vancouver");
+        Elements listOfEventNames = null;
+        for (Element event : content) {
+            listOfEventNames = event.getElementsByTag("h3");
+        }
+        return listOfEventNames;
+    }
+
+    /**
+     * Get all the event information from the article
+     *
+     * @return A list of Element with all event info
+     */
+    public Elements getEventInformation() {
+        Elements content = doc.getElementsByClass("post content vancouver");
+        Elements eventInfo = null;
+        for (Element event : content) {
+            eventInfo = event.getElementsByTag("ul");
+        }
+        return eventInfo;
+    }
+
+    /**
+     * Allows user to change the url of the article, the doc is immediately updated as well.
+     * Throws an IO exception if the URL does not allow for successful creation of a Document.
+     * @param newURL
+     *
+     * */
+    public void changeURL(String newURL) throws IOException{
+        this.articleURL = newURL;
+        this.doc = Jsoup.connect(this.articleURL).get();
     }
 
 }
